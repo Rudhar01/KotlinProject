5fa8308a5de8683baf8d5d38effc175d1eed4a1b
Rudhar01 <rudharthakur74@gmail.com>
2025-06-28 13:36:11 +0530

    Add files via upload

---
+++ SignIn.kt
@@ -0,0 +1,32 @@
+package com.example.kotlinproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignIn : AppCompatActivity() {
+    lateinit var  name : TextView
+    lateinit var pass : TextView
+    lateinit var button : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        name=findViewById(R.id.EnterName)
+        pass=findViewById(R.id.EnterPass)
+        button=findViewById(R.id.ButtonIn)
+        button.setOnClickListener{
+
+        }
+    }
+}
